6ae61ca8c44ef4e29947a31e8d6b7cae3b98e10a
urtesala <urtesaladziute@gmail.com>
2022-12-05 10:54:07 +0300

    singel user page with allParams / useParams

--- src/pages/SingleUserPage.jsx
+++ src/pages/SingleUserPage.jsx
@@ -1,9 +1,20 @@
+import { useParams } from 'react-router-dom';
 import { Section, Title } from '../components/UI.styled';
 
 function SingleUserPage(props) {
+  const allParams = useParams();
+  //   console.log('allParams ===', allParams);
+  //   console.log('userId ===', allParams.userId);
+  const currentUserId = allParams.userId;
+
+  //susikurti data state for current user currentUser
+  //parsisiusti su fetch useEffect duomenis apie konkretu user
+  // is gautu duomenu atvaizduojam daugiau info apie useri
+
   return (
     <Section>
       <Title>Single user page</Title>
+      <p>I should load user with id: {currentUserId}</p>
     </Section>
   );
 }
