09727cec5c886789652a5e5be8af6c4a9bf6dd4a
urtesala <urtesaladziute@gmail.com>
2022-12-05 09:57:47 +0300

    local data

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Section } from '../components/UI.styled';
 
 function UsersPage(props) {
@@ -11,10 +12,21 @@ function UsersPage(props) {
 
   async function getData() {
     try {
-      const url = 'https://dummyjson.com/users';
+      let url = 'https://dummyjson.com/users';
+      url = '/api/users.json';
+
       const resp = await fetch(url);
       const dataInJs = await resp.json();
-      console.log('dataInJs ===', dataInJs);
+
+      //   const usersWithNameAndId = dataInJs.users.map((uOb) => ({
+      //     id: uOb.id,
+      //     firstName: uOb.firstName,
+      //     lastName: uOb.lastName,
+      //     age: uOb.age,
+      //   }));
+      //   console.log(JSON.stringify(usersWithNameAndId));
+
+      //   console.log('dataInJs ===', dataInJs.users);
     } catch (error) {
       console.warn('klaida getData', error);
     }
