0b21e696401b77cfbc7c9c43d6d7b2ed1ecb77bc
urtesala <urtesaladziute@gmail.com>
2022-12-05 10:18:31 +0300

    mapping users list in li

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -2,10 +2,9 @@ import { useEffect, useState } from 'react';
 import { Section } from '../components/UI.styled';
 
 function UsersPage(props) {
-  //susikurti usersArr state
-
-  const [userArr, setUserArr] = useState([]);
-  //useEffect supildyt state su duomenimis is dummyjson.com/users
+  // susikurti usersArr state
+  const [usersArr, setUsersArr] = useState([]);
+  // su useEffect supildyti state su duomenimis is https://dummyjson.com/users
   useEffect(() => {
     getData();
   }, []);
@@ -14,19 +13,13 @@ function UsersPage(props) {
     try {
       let url = 'https://dummyjson.com/users';
       url = '/api/users.json';
-
       const resp = await fetch(url);
       const dataInJs = await resp.json();
 
-      //   const usersWithNameAndId = dataInJs.users.map((uOb) => ({
-      //     id: uOb.id,
-      //     firstName: uOb.firstName,
-      //     lastName: uOb.lastName,
-      //     age: uOb.age,
-      //   }));
-      //   console.log(JSON.stringify(usersWithNameAndId));
-
-      //   console.log('dataInJs ===', dataInJs.users);
+      // dataInJs === {users: Array(30), total: 100, skip: 0, limit: 30}
+      console.log('dataInJs ===', dataInJs.users);
+      // set state
+      setUsersArr(dataInJs.users);
     } catch (error) {
       console.warn('klaida getData', error);
     }
@@ -34,9 +27,26 @@ function UsersPage(props) {
 
   return (
     <Section>
-      <h1>Users Page</h1>
-      <p>this is our users</p>
+      <h1>UsersPage</h1>
+      <p>This is our users</p>
+
+      <ol>
+        {/* is usersArr pagaminti li elementus su vardu ir pavarde */}
+        {usersArr.map((uObj) => (
+          <li key={uObj.id}>
+            {uObj.firstName} {uObj.lastName}
+          </li>
+        ))}
+      </ol>
     </Section>
   );
 }
 export default UsersPage;
+
+// const usersWithNameAndId = dataInJs.users.map((uOb) => ({
+//   id: uOb.id,
+//   firstName: uOb.firstName,
+//   lastName: uOb.lastName,
+//   age: uOb.age,
+// }));
+// console.log(JSON.stringify(usersWithNameAndId));
