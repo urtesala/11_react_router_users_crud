b085f174d44c21ca60d690af6c307fe3e48f6d22
urtesala <urtesaladziute@gmail.com>
2022-12-05 09:39:44 +0300

    get data try and catch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Section from '../components/UI.styled';
+import { Section } from '../components/UI.styled';
 
 const Title = styled.h1`
   font-size: 3rem;
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,8 +1,24 @@
-import Section from '../components/UI.styled';
+import { Section } from '../components/UI.styled';
 
 function UsersPage(props) {
   //susikurti usersArr state
+
+  const [userArr, setUserArr] = useState([]);
   //useEffect supildyt state su duomenimis is dummyjson.com/users
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    try {
+      const url = 'https://dummyjson.com/users';
+      const resp = await fetch(url);
+      const dataInJs = await resp.json();
+      console.log('dataInJs ===', dataInJs);
+    } catch (error) {
+      console.warn('klaida getData', error);
+    }
+  }
 
   return (
     <Section>
